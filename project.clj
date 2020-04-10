37233097f6006b63c52dd87fb8c1fb5e0fc4b35d
Nick (Git client) <nick@cassiel.com>
2020-04-10 11:04:55 +0100

    More setup.

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.597"]
                                   [com.bhauman/figwheel-main "0.2.3"]
                                   ;; optional but recommended
-                                  [com.bhauman/rebel-readline-cljs "0.1.4"]]}})
+                                  [com.bhauman/rebel-readline-cljs "0.1.4"]]
+                   :resource-paths ["target"]
+                   :clean-targets ^{:protect false} ["target"]}}
+  :aliases {"fig" ["trampoline" "run" "-m" "figwheel.main"]})
