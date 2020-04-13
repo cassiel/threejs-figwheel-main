ec86717dfa76ca0cc0c96b2c8d9499fafb4a35bb
Nick (Git client) <nick@cassiel.com>
2020-04-13 15:29:07 +0100

    More rendering.

--- src/hello_world/sculpture.cljs
+++ src/hello_world/sculpture.cljs
@@ -24,6 +24,36 @@
     (->> (repeatedly (inc z) strip-row-fn)
          (apply geom/group-spaced-by [0 0 CELL-PITCH]))))
 
+(defn rand-colour []
+  (js/THREE.Color. (str "hsl(" (* (rand) 360) ", 100%, 50%)")))
+
+(def light-geometry (js/THREE.SphereGeometry. 0.1 32 32))
+(defn light-material [c] (js/THREE.MeshPhongMaterial. (clj->js {:color c
+                                                                :wireframe false})))
+
+(defn light-matrix
+  "Light source generation. We can't draw too many lights in a scene, so we
+   fudge by making most of them inert and black."
+  []
+  (let [light-source-fn #(let [light? (> (rand) 0.98)
+                               c (if light?
+                                   #_ (js/THREE.Color. (rand) (rand) (rand))
+                                   (rand-colour)
+                                   (js/THREE.Color. 0x000000))
+                               marker (js/THREE.Mesh. light-geometry
+                                                      (light-material c))]
+                           (if light?
+                             (geom/group marker
+                                         (js/THREE.PointLight. c 0.5 0.001 5))
+                             marker))
+        light-strip-x-fn #(->> (repeatedly CELLS-X light-source-fn)
+                               (apply geom/group-spaced-by [CELL-PITCH 0 0]))
+        light-plane-z-fn #(->> (repeatedly CELLS-Z light-strip-x-fn)
+                               (apply geom/group-spaced-by [0 0 CELL-PITCH]))
+        ]
+    (->> (repeatedly CELLS-Y light-plane-z-fn)
+         (apply geom/group-spaced-by [0 CELL-PITCH 0]))))
+
 (defn form []
   (let [red (js/THREE.MeshPhongMaterial. (clj->js {:color 0xFF8080
                                                    :wireframe false}))
@@ -31,7 +61,7 @@
                                                      :wireframe false}))
         blue (js/THREE.MeshPhongMaterial. (clj->js {:color 0x8080FF
                                                     :wireframe false}))
-        dark (js/THREE.MeshPhongMaterial. (clj->js {:color 0x606060
+        dark (js/THREE.MeshPhongMaterial. (clj->js {:color 0xFFFFFF
                                                     :wireframe false}))
         obj (->> (vertical dark CELLS-X CELLS-Y CELLS-Z)
                  (geom/shift [(/ BAR-WIDTH 2) 0 0]))
@@ -56,8 +86,9 @@
                 obj-rz
                 #_ (js/THREE.Mesh. (js/THREE.SphereGeometry. 0.1 32 32)
                                 material)
-                (geom/shift [0 2 0] mini-light-1)
-                (geom/shift [0 -2 0] mini-light-2)
+                #_ (geom/shift [0 2 0] mini-light-1)
+                #_ (geom/shift [0 -2 0] mini-light-2)
+                (light-matrix)
                 #_ (geom/shift [0 10 0] light2)
                 #_ (geom/shift [10 0 0] light3)
                 #_ (geom/shift [10 0 10] dlight))))
