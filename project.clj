19a4d586046a7daedf5f27bc6280a5bc484a35a7
Nick (Git client) <nick@cassiel.com>
2023-04-19 12:09:11 +0100

    Version bump (though with old OrbitControls).

--- project.clj
+++ project.clj
@@ -1,15 +1,21 @@
 (defproject threejs-figwheel-main "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.3"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.11.4"]
-                                  [com.bhauman/figwheel-main "0.2.16"]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.11.60"]
+                                  [com.bhauman/figwheel-main "0.2.18"]
                                   ;; optional but recommended
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]
 
-                                  [com.stuartsierra/component "1.0.0"]
+                                  [com.stuartsierra/component "1.1.0"]
                                   [net.cassiel/lifecycle "0.1.0-SNAPSHOT"]
 
                                   [cljsjs/stats "16.0-0"]]
                    :resource-paths ["target"]
                    :clean-targets ^{:protect false} ["target"]}}
-  :plugins [[lein-ancient "1.0.0-RC3"]]
+  :plugins [[lein-cljsbuild "1.1.8"]
+            ;; These seem pretty equivalent:
+            [lein-ancient "1.0.0-RC3"]
+            [com.github.liquidz/antq "RELEASE"]]
+  :cljsbuild {:builds [{:source-paths ["src"]
+                        :compiler {:output-to "resources/public/js/_COMPILED.js"
+                                   :optimizations :whitespace}}]}
   :aliases {"fig" ["trampoline" "run" "-m" "figwheel.main"]})
