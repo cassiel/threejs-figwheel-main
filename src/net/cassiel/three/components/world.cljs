3e0494d1c8d1edcd24f815a52a94743b3e770236
Nick (Git client) <nick@cassiel.com>
2023-07-04 20:37:46 +0100

    First cut: hand-rolled geometries.

--- src/net/cassiel/three/components/world.cljs
+++ src/net/cassiel/three/components/world.cljs
@@ -4,6 +4,7 @@
             [net.cassiel.three.forms.cube :as cube]
             [net.cassiel.three.forms.haüy :as haüy]
             [net.cassiel.three.forms.printed :as printed]
+            [net.cassiel.three.forms.tiles :as tiles]
             [net.cassiel.three.forms.sculpture :as sculpture]))
 
 (defrecord WORLD [scene renderer stopper stats installed?]
@@ -23,7 +24,7 @@
                                                                  1000)
                              renderer (js/THREE.WebGLRenderer.)
                              controls (js/THREE.OrbitControls. camera (.-domElement renderer))
-                             content (printed/form)
+                             content (tiles/form)
                              ;; An "alive" flag to let us kill the animation
                              ;; refresh when we tear down:
                              RUNNING (atom true)]
---
+++ src/net/cassiel/three/forms/tiles.cljs
@@ -0,0 +1,64 @@
+(ns net.cassiel.three.forms.tiles
+  "Manual assembly of a mesh via BufferGeometry.
+   Lifted largely from https://github.com/mrdoob/three.js/blob/master/examples/webgl_buffergeometry.html"
+  (:require [net.cassiel.three.geom :as geom]
+            [oops.core :refer [oget oget+ oset! oset!+ ocall oapply]]))
+
+(defn form []
+  (let [frame (js/THREE.Mesh. (js/THREE.BoxGeometry. 4 4 4)
+                              (js/THREE.MeshBasicMaterial. (clj->js {:color     0x808080
+                                                                     :wireframe true})))
+
+        geometry (js/THREE.BufferGeometry.)
+
+        [ax ay az] [-2 -2 -2]
+        [bx by bz] [-2 2 2]
+        [cx cy cz] [2 -2 2]
+
+        positions #js [ax ay az bx by bz cx cy cz]
+
+        ;; flat face normals
+        pA (js/THREE.Vector3. ax ay az)
+        pB (js/THREE.Vector3. bx by bz)
+        pC (js/THREE.Vector3. cx cy cz)
+
+        cb (js/THREE.Vector3.)
+        ab (js/THREE.Vector3.)
+
+        _ (do (ocall cb :subVectors pC pB)
+              (ocall ab :subVectors pA pB)
+              (ocall cb :cross ab)
+              (ocall cb :normalize))
+
+        nx (oget cb :x)
+        ny (oget cb :y)
+        nz (oget cb :z)
+
+        normals #js [nx ny nz nx ny nz nx ny nz]
+        colours #js [0.5 0.5 0.5 0.5
+                     0.5 0.5 0.5 0.5
+                     0.5 0.5 0.5 0.5]
+
+        ;; Skip array disposal for now.
+
+        _ (doto geometry
+            (ocall :setAttribute "position" (js/THREE.Float32BufferAttribute. positions 3))
+            (ocall :setAttribute "normal" (js/THREE.Float32BufferAttribute. normals 3))
+            (ocall :setAttribute "color" (js/THREE.Float32BufferAttribute. colours 4))
+            (ocall :computeBoundingSphere)
+            )
+
+        material (js/THREE.MeshPhongMaterial. #js {:color        0xD5D5D5
+                                                   :specular     0xFFFFFF
+                                                   :shininess    250
+                                                   :side         js/THREE.DoubleSide
+                                                   :vertexColors true
+                                                   :transparent  true
+                                                   :wireframe    false})
+
+        mesh (js/THREE.Mesh. geometry material)
+        ]
+
+    (geom/group frame
+                mesh
+                (geom/shift [0 0 5] (js/THREE.DirectionalLight. 0xFFFFFF 1)))))
